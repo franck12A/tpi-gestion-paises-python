0ec603d20d22c4b60b1e66fcfdde461ce8cb7ef4
FrancoEnrici <125381336+franck12A@users.noreply.github.com>
2026-06-15 20:02:40 -0300

    Actualizar modulo de datos

--- datos.py
+++ datos.py
@@ -1,10 +1,11 @@
 import csv
+from validaciones import validar_texto, validar_entero, validar_opcion, validar_continente
 
 def agregar_pais(paises):
-    nombre = input("Nombre del país: ")
-    poblacion = int(input("Población: "))
-    superficie = int(input("Superficie: "))
-    continente = input("Continente: ")
+    nombre = validar_texto("Nombre del país: ")
+    poblacion = validar_entero("Población: ")
+    superficie = validar_entero("Superficie: ")
+    continente = validar_continente("Continente: ")
 
     pais = {
         "nombre": nombre,
@@ -18,14 +19,14 @@ def agregar_pais(paises):
     print("País agregado correctamente.")
 
 def actualizar_pais(paises):
-    nombre_buscado = input("Ingrese el nombre del país a actualizar: ")
+    nombre_buscado = validar_texto("Ingrese el nombre del país a actualizar: ")
 
     for pais in paises:
         if pais["nombre"].lower() == nombre_buscado.lower():
-            print("País encontrado. Ingrese los nuevos datos (deje en blanco para mantener el valor actual):")
+            print("País encontrado. Ingrese los nuevos datos: ")
 
-            nueva_poblacion = input(f"Población actual ({pais['poblacion']}): ")
-            nueva_superficie = input(f"Superficie actual ({pais['superficie']}): ")
+            nueva_poblacion = validar_entero(f"Población actual ({pais['poblacion']}): ")
+            nueva_superficie = validar_entero(f"Superficie actual ({pais['superficie']}): ")
 
             pais["poblacion"] = nueva_poblacion
             pais["superficie"] = nueva_superficie
@@ -55,3 +56,11 @@ def cargar_csv(nombre_archivo):
         print("No se encontró el archivo CSV.")
 
     return paises
+
+def guardar_csv(nombre_archivo, paises):
+    with open(nombre_archivo, mode="w", newline="", encoding="utf-8") as archivo:
+        archivo.write("nombre,poblacion,superficie,continente\n")
+
+        for pais in paises:
+            linea = ",".join([pais["nombre"], str(pais["poblacion"]), str(pais["superficie"]), pais["continente"]]) + "\n"
+            archivo.write(linea + "\n")
